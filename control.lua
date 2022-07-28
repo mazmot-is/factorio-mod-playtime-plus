631be6a055cd4b6814f0d921718136465ac8041b
mazmot <mazmot-is@users.noreply.github.com>
2022-07-28 20:34:30 +0900

    change location when mod setting changed

--- control.lua
+++ control.lua
@@ -5,7 +5,7 @@ local function update_timer()
   local seconds = math.floor(base) % 60
   local minutes = math.floor(base/60) % 60
   local hours = math.floor(base/3600)
-  local label = ptplus.frame.ptplus_label
+  local label = ptplus.frame["ptplus-label"]
   if hours > 0 then
     label.caption = string.format("%d:%02d:%02d", hours, minutes, seconds)
   else
@@ -14,27 +14,27 @@ local function update_timer()
 end
 
 local function create_gui(player)
-  local frame = player.gui.screen.add{type="frame", name="ptplus_frame"}
+  local frame = player.gui.screen.add{type="frame", name="ptplus-frame"}
   ptplus.frame = frame
   frame.style.padding = {0, 6, 0, 6}
-  local label = frame.add{type="label", name="ptplus_label"}
+  local label = frame.add{type="label", name="ptplus-label"}
   label.drag_target = frame
 end
 
 local function init_gui()
   reset_gui_location()
-  save_gui_location()
-  save_gui_location()
   update_timer()
 end
 
 function reset_gui_location()
   ptplus.frame.location = {settings.global["ptplus-x"].value, settings.global["ptplus-y"].value}
+  save_gui_location()
+  save_gui_location() -- call twice to override buffering
 end
 
 function save_gui_location()
   -- buffering gui location to look one previous location due to
-  -- on_gui_location_changed is triggerd AFTER on_player_display_resolution_change
+  -- on_gui_location_changed() is triggerd AFTER on_player_display_resolution_change()
   ptplus.x1 = ptplus.x2
   ptplus.y1 = ptplus.y2
   ptplus.x2 = ptplus.frame.location.x
@@ -62,16 +62,17 @@ end
 
 local function on_player_created(event)
   create_gui(game.players[event.player_index])
+  init_gui()
 end
 
 local function on_nth_tick_1() -- an altenative to on_init() and on_load() to access game.players[1]
   script.on_nth_tick(1, nil) -- remove myself immediately to run at once
 
   -- if gui not eixsts (e.g., load saved data that did not use ptplus)
-  if not game.players[1].gui.screen.ptplus_frame then
+  if not game.players[1].gui.screen["ptplus-frame"] then
      create_gui(game.players[1])
   end
-  ptplus.frame = game.players[1].gui.screen.ptplus_frame
+  ptplus.frame = game.players[1].gui.screen["ptplus-frame"]
   init_gui()
 end
 
@@ -80,21 +81,25 @@ local function on_nth_tick_60()
 end
 
 local function on_gui_click(event)
-  if event.element.name == "ptplus_label" and event.button == defines.mouse_button_type.right then
+  if event.element.name == "ptplus-label" and event.button == defines.mouse_button_type.right then
     reset_gui_location()
-    save_gui_location()
-    save_gui_location() -- call twice to override buffering
   end
 end
 
 local function on_gui_location_changed(event)
-  if event.element.name == "ptplus_frame" then
+  if event.element.name == "ptplus-frame" then
     save_gui_location()
   end
 end
 
 local function on_player_display_resolution_changed(event)
   reposition_gui(event)
+end
+
+local function on_runtime_mod_setting_changed(event)
+  if event.setting == "ptplus-x" or event.setting == "ptplus-y" then
+    reset_gui_location()
+  end
 end
 
 --------------------------------------------------------------------------------
@@ -105,4 +110,4 @@ script.on_nth_tick(60, on_nth_tick_60)
 script.on_event(defines.events.on_gui_click, on_gui_click)
 script.on_event(defines.events.on_gui_location_changed, on_gui_location_changed)
 script.on_event(defines.events.on_player_display_resolution_changed, on_player_display_resolution_changed)
-
+script.on_event(defines.events.on_runtime_mod_setting_changed, on_runtime_mod_setting_changed)
