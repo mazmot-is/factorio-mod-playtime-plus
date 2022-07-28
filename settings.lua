d9aa3f7851341f43fc8e91445dd9069433ea13f9
mazmot <mazmot-is@users.noreply.github.com>
2022-07-28 18:30:18 +0900

    forgot to add settings.lua

---
+++ settings.lua
@@ -0,0 +1,16 @@
+data:extend({
+    {
+      type = "int-setting",
+      name = "ptp-x",
+      setting_type = "runtime-global",
+      minimum_value = 0,
+      default_value = 270,
+    },
+    {
+      type = "int-setting",
+      name = "ptp-y",
+      setting_type = "runtime-global",
+      minimum_value = 0,
+      default_value = 0,
+    },
+})
