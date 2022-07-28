720f1d67d5118c276ec5a16475ac9819be41ebf2
mazmot <mazmot-is@users.noreply.github.com>
2022-07-28 18:27:33 +0900

    more stable draggable

--- control.lua
+++ control.lua
@@ -1,18 +1,15 @@
-if not ptp then ptp = {} end
+script.on_init(function()
+    global.ptp = {}
+end)
 
-function update_timer()
-  -- nothing to do if no player
-  if not game then return end
-  if not game.players[1] then return end
 
-  -- generate gui
-  local label = get_ptp_label()
+local function update_timer()
+  local label = global.ptp.frame.ptp_label
   
-  -- update timer
-  local sec = math.floor(game.tick/60)
-  local seconds = math.floor(sec) % 60
-  local minutes = math.floor(sec/60) % 60
-  local hours = math.floor(sec/3600)
+  local basesec = math.floor(game.tick/60)
+  local seconds = math.floor(basesec) % 60
+  local minutes = math.floor(basesec/60) % 60
+  local hours = math.floor(basesec/3600)
   if hours > 0 then
     label.caption = string.format("%d:%02d:%02d", hours, minutes, seconds)
   else
@@ -20,63 +17,85 @@ function update_timer()
   end
 end
 
-function get_ptp_label()
-  local screen = game.players[1].gui.screen
-  if not screen.ptp_frame then
-    local frame = screen.add{type="frame", name="ptp_frame"}
-    frame.style.padding = {0, 6, 0, 6}
-    frame.location = {540, 0}
-    local label = frame.add{type="label", name="ptp_label"}
-    label.drag_target = frame
-  end
-  return screen.ptp_frame.ptp_label
+local function create_gui(event)
+  if not game.players[event.player_index] then return end -- satd. prevent error on tutorial
+
+  local player = game.players[event.player_index]
+  local screen = player.gui.screen
+
+  global.ptp.frame = screen.add{type="frame", name="ptp_frame"}
+  global.ptp.frame.style.padding = {0, 6, 0, 6}
+  global.ptp.frame.location = {540, 0}
+
+  local label = global.ptp.frame.add{type="label", name="ptp_label"}
+  label.drag_target = global.ptp.frame
+
+  save_gui_location()
+  save_gui_location()
+  update_timer()
 end
 
-function save_frame_location()
-  if not game then return end
-  if not game.players[1] then return end
-  ptp.x = game.players[1].gui.screen.ptp_frame.location.x
-  ptp.y = game.players[1].gui.screen.ptp_frame.location.y
-  game.print(string.format("%d:%d ", x, y))
+local function create_gui2()
+  local e = {}
+  e.player_index = 1
+  create_gui(e)
 end
 
-script.on_init(update_timer)
-script.on_load(update_timer)
+local function reset_gui_location()
+  global.ptp.frame.location = {540, 0}
+end
+
+function save_gui_location()
+  -- buffering gui location to look one previous location due to
+  -- on_gui_location_changed is triggerd AFTER on_player_display_resolution_change
+  global.ptp.x1 = global.ptp.x2
+  global.ptp.y1 = global.ptp.y2
+  global.ptp.x2 = global.ptp.frame.location.x
+  global.ptp.y2 = global.ptp.frame.location.y
+  game.print(string.format("a %d:%d ", global.ptp.x2, global.ptp.y2))
+end
+
+local function reposition_gui(event)
+  if not global.ptp.x1 then return end
+
+  local player = game.players[event.player_index]
+  local currw = player.display_resolution.width
+  local currh = player.display_resolution.height
+  local prevw = event.old_resolution.width
+  local prevh = event.old_resolution.height
+  local newx = math.floor(global.ptp.x1 / prevw * currw)
+  local newy = math.floor(global.ptp.y1 / prevh * currh)
+  global.ptp.frame.location = {newx, newy}
+
+  -- update location
+  save_gui_location()
+  save_gui_location() -- call twice to override buffering
+end
+
+
+--script.on_event(defines.events.on_player_created, save_gui_location)
+--script.on_event(defines.events.on_player_created, save_gui_location) -- call twice to override buffering
+script.on_event(defines.events.on_player_created, create_gui)
+script.on_configuration_changed(create_gui2) -- a case where the mod has been applied for existing save
+
 script.on_nth_tick(60, update_timer)
 
-script.on_init(save_frame_location)
-script.on_load(save_frame_location)
-
 script.on_event(defines.events.on_gui_click, function(event)
-  if event.element.name == "ptp_label" then
-    x = game.players[1].gui.screen.ptp_frame.location.x
-    y = game.players[1].gui.screen.ptp_frame.location.y
+  if event.element.name == "ptp_label" and event.button == defines.mouse_button_type.right then
+    reset_gui_location()
+    save_gui_location()
+    save_gui_location() -- call twice to override buffering
   end
 end)
 
 script.on_event(defines.events.on_gui_location_changed, function(event)
   if event.element.name == "ptp_frame" then
-    x = game.players[1].gui.screen.ptp_frame.location.x
-    y = game.players[1].gui.screen.ptp_frame.location.y
+    save_gui_location()
   end
 end)
 
 script.on_event(defines.events.on_player_display_resolution_changed, function(event)
-  local player = game.players[1]
-  local w = player.display_resolution.width
-  local h = player.display_resolution.height
-  local x = player.gui.screen.ptp_frame.location.x
-  local y = player.gui.screen.ptp_frame.location.y
-  game.print(string.format("%d:%d  %d:%d", w, h, x, y))
+  reposition_gui(event)
 end)
 
 
--- reset position
---script.on_event(defines.events.on_gui_click, function(event)
---  if event.element.name == "ptp_label" then
---    local x = game.players[1].gui.screen.ptp_frame.location.x
---    local y = game.players[1].gui.screen.ptp_frame.location.y
---    game.print(string.format("%d:%d", x, y))
---    game.players[1].gui.screen.ptp_frame.location = {540, 0}
---  end
---end)
